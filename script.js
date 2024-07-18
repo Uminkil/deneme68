2cb5df3247dd5158af5c79a4bbc252caae83cbea
Uminkil <131275020+Uminkil@users.noreply.github.com>
2024-07-18 10:14:07 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('dealsClosed').innerText = '$5,643';
+    document.getElementById('dealsClosedProgress').style.width = '112%';
+
+    document.getElementById('currentPipeline').innerText = '$17.9K';
+    document.getElementById('currentPipelineProgress').style.width = '89%';
+
+    document.getElementById('topOpportunities').innerText = '$44.7K';
+    document.getElementById('topOpportunitiesProgress').style.width = '74%';
+});
